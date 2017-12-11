2c06f6269511e26cffd90554ffd99d3380ed3b75
Ben <reidben24@gmail.com>
2017-12-11 10:41:27 -0500

    Init repo

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>xkcd Reddit Stats</title>
+</head>
+
+<body>
+	<h1>xkcd Reddit Stats</h1>
+	<p>Eventually I will deploy the website for xkcd Reddit stats here</p>
+</body>
+
+</html>
